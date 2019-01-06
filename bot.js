65323e58887511f14d7344c3b1c3d66cb9f4f752
faresmnasri <31959540+faresmnasri@users.noreply.github.com>
2019-01-06 18:09:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,4 +17,22 @@ client.on("guildMemberAdd", member => {
   })
 
   
+client.on('guildMemberAdd', Sal => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(Sal.user.username, Sal.user.avatarURL)
+    .setThumbnail(Sal.user.avatarURL)
+    .setImage('https://cdn.discordapp.com/attachments/529659606419374081/531406992909139968/1line.gif')
+    .setTitle('عضو جديد!')
+    .setDescription('MAR7BA BIK FI SERVER )
+    .addField('``YOUR ID``:',"" +  Sal.user.id, true)
+    .addField('``YOUR TAG``', Sal.user.discriminator, true)
+    .addField('``YOU CREATED AT ``', Sal.user.createdAt, true)
+    .addField(' 👤  YOU ARE THE MEMBER NUMBER',`**[ ${Sal.guild.memberCount} ]**`,true)
+    .setColor('RANDOM')
+    .setFooter(Sal.guild.name, Sal.guild.iconURL, true)
+    var channel =Sal.guild.channels.find('【welcome】', 'welcome')
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
+
   client.login(process.env.BOT_TOKEN);
